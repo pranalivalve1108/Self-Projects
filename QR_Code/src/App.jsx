b7388d7ceda5123efe3778d4ece3ac7c2bd04191
pranalivalve1108 <pranalivalve2001@gmail.com>
2024-08-04 19:13:32 +0530

    first commit

---
+++ QR_Code/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card';
+
+function App() {
+
+  return (
+    
+    <center className='container'>
+      <Card></Card>
+</center>
+   
+  );;
+};
+
+export default App;
---
+++ QR_Code/src/components/Card.jsx
@@ -0,0 +1,14 @@
+const Card = () => {
+
+    return (
+    <div className="card card1" style={{width: "18rem"}}>
+  <img src="./src/assets/qr code.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title name">Pranali Valve</h5>
+    <p className="card-text">Computer Engineering student seeking full time job in Frontend Developer | Web Developer | Full Stack Developer.</p>
+    <a href="https://www.linkedin.com/in/pranali-valve-68b1ba1a0/" className="btn btn-primary">Click Here</a>
+  </div>
+</div>
+    );
+};
+export default Card;
